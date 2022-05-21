8907332fcaa10b81a453bc38df6a64af4b95f7ac
Ayomide Fayomi <storario2@gmail.com>
2022-05-21 14:43:28 +0100

    conflict clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
 import { Home, Footer, About, Header, Pricing, Contact, StartCampaign } from "./components/common";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -25,39 +23,3 @@ function App() {
 }
 
 export default App;
-=======
-import React from "react";
-import {Header} from "./components/common"
-import './App.css';
-import {Home} from "./components/common";
-import {Footer} from "./components/common";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {About} from "./components/common";
-import {Pricing} from "./components/common";
-import {Contact} from "./components/common";
-import ScrollToTop from "./components/ScrollToTop";
-import { Details } from "./components/common";
-
-
-
-function App() {
-  return (
-      <BrowserRouter>
-      <ScrollToTop />
-        <div className='App-header'>
-           <Header />
-              <Routes>
-                <Route path="/" exact element={<Home />} />
-                <Route path="/about" exact element={<About />} />
-                <Route path="/pricing" exact element={<Pricing />} />
-                <Route path="/contact" exact element={<Contact />} />
-                <Route path="/details" exact element={<Details />} />
-              </Routes>
-           <Footer />
-        </div>
-      </BrowserRouter>
-  );
-}
-
-export default App;
->>>>>>> 0cdd4486995db036177bd3d36e32fa66b782497c
--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,72 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { Button } from 'react-bootstrap';
-import { Navbar } from 'react-bootstrap';
-import { Nav } from 'react-bootstrap';
-import { Form, Container, Offcanvas } from 'react-bootstrap';
-import {Link} from 'react-router-dom';
-
-import './Header.css';
-
-const Header = () => <main>
-        <Navbar bg="transparent" className="desktop" expand="lg" fixed="top">
-          <div className="offcanvas-header">  
-            <button className="btn-close float-end"></button>
-        </div>
-          <Navbar.Brand as={Link} to="/" exact="true"><img src="logo-black.png" alt="brand-logo" width={'150px'} /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
-              navbarScroll
-            >
-              <Nav.Link as={Link} to="/startcampaign" >
-                Start
-              </Nav.Link>
-              <Nav.Link as={Link} to="/" >
-                Team
-              </Nav.Link>
-              <Nav.Link as={Link} to="/" >Our Mission</Nav.Link>          
-            </Nav>
-            <Form className="d-flex">
-              <Button variant="outline-success" as={Link} to="/">Connect Wallet</Button>
-            </Form>
-          </Navbar.Collapse>
-      </Navbar>
-      <Container className="mobile">
-      <Navbar bg="transparent"  fixed="top" collapseOnSelect expand="false" >
-           <Navbar.Brand as={Link} to="/" exact="true"><img src="16.png" alt="brand-logo" width={'150px'} /></Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Offcanvas
-              id="offcanvasNavbar"
-              aria-labelledby="offcanvasNavbarLabel"
-              placement="end"
-            >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel"><img src="logo-new.png" alt="brand-logo" width={'250px'} /></Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link as={Link} to="/solution" eventKey="3" >
-                Start
-              </Nav.Link>
-              <Nav.Link as={Link} to="/pricing" eventKey="4">
-                Team
-              </Nav.Link>
-              <Nav.Link as={Link} to="/about" eventKey="5">Our Mission</Nav.Link>          
-            </Nav>
-            <Form className="d-flex">
-              <Nav.Link className="outline-success" as={Link} to="/contact" eventKey="6">Connect Wallet</Nav.Link>
-            </Form>
-            </Offcanvas.Body>
-       </Navbar.Offcanvas>
-      </Navbar>
-      </Container>
-</main>
-
-=======
-import React from "react";
 import { Button } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
@@ -91,7 +22,7 @@ const Header = () => <main>
               <Nav.Link as={Link} to="/" >
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/" >
+              <Nav.Link as={Link} to="/startcampaign" >
                 Start
               </Nav.Link>
               <Nav.Link as={Link} to="/about" >About Us</Nav.Link>          
@@ -115,7 +46,7 @@ const Header = () => <main>
             </Offcanvas.Header>
             <Offcanvas.Body>
             <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link as={Link} to="/solution" eventKey="3" >
+              <Nav.Link as={Link} to="/startcampaign" eventKey="3" >
                 Start
               </Nav.Link>
               <Nav.Link as={Link} to="/pricing" eventKey="4">
@@ -131,6 +62,4 @@ const Header = () => <main>
       </Navbar>
       </Container>
 </main>
-
->>>>>>> 0cdd4486995db036177bd3d36e32fa66b782497c
 export default Header;
